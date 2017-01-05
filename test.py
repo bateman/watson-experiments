ed3da111c34f277fcbc2d3c90ebf2112f3c16bd1
Fabio Calefato <fcalefato@gmail.com>
2017-01-05 17:32:15 +0100

    simple hello world test for watson services

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ tone_analyzer = ToneAnalyzerV3(
     username='d6739ba1-970e-4b5a-9e38-f037fde2b299',
     password='z7pTaPSvgx8T',
     version='2016-02-11')
-js = tone_analyzer.tone(text='I am very happy')
+js = tone_analyzer.tone(text='I am very happy', tones='social', sentences=False)
 print(json.dumps(js, indent=2))
