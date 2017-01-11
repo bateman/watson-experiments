2a8068b18e984d1129564ffb848dc9be5eaf964d
Fabio Calefato <fcalefato@gmail.com>
2017-01-11 12:42:00 +0100

    final changes to compute the simple logit

--- simple-log-reg.r
+++ simple-log-reg.r
@@ -1,9 +1,5 @@
-setwd("C:\\Users\\Fabio\\Documents\\GitHub\\watson-experiments")
-
 dataset <- read.csv2('logit-dataset.csv')
 dataset = na.omit(dataset)
-#dataset$PropensityToTrust <- as.numeric(dataset$PropensityToTrust)
-#logit <- glm(formula=Successful ~PropensityToTrust, data=dataset, family=binomial())
 
 logit <- glm(formula=Successful ~I(PropensityToTrust=='HIGH'), data=dataset, family=binomial())
 res = summary(logit)
@@ -21,3 +17,8 @@ dataset$prob=prob
 g <- roc(Successful ~ prob, data = dataset)
 #visualizzazione della curva
 plot(g)
+
+newdata = data.frame(PropensityToTrust='HIGH')
+predict.glm(logit, newdata, type="response")
+newdata = data.frame(PropensityToTrust='LOW')
+predict.glm(logit, newdata,type="response")
